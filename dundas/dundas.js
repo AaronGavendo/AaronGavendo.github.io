2e933f74f7f8ad8ee0c270361153a267b65e483f
AaronGavendo <agavendo@hotmail.com>
2016-06-16 23:02:25 -0400

    Update Dundas for mobile test

--- dundas/dundas.js
+++ dundas/dundas.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
 var nameArr = [];
 var valuArr = [];
 var alphanum = /^[0-9a-zA-Z]+$/;
+var selectedOption = -1;
+
+
 
 	//When the Add button is pressed
 	$("#add").click(function () {
@@ -24,7 +27,7 @@ var alphanum = /^[0-9a-zA-Z]+$/;
 			arLen = valuArr.length;
 
 			for (i = 0; i < arLen; i++) {
-    			text += "<option>" + nameArr[i] + "=" + valuArr[i] + "</option>";
+				text += "<option id=\"option" + i +  "\">" + nameArr[i] + "=" + valuArr[i] + "</option>";
 			}
 			document.getElementById("list1").innerHTML = text;
 		}
@@ -34,6 +37,7 @@ var alphanum = /^[0-9a-zA-Z]+$/;
 		}
 	});
 
+	//Sort by name
 	$("#sortName").click(function () {
 		//Create new array. Splice two arrays. Then sort by name.
 		var sna = [];
@@ -42,7 +46,6 @@ var alphanum = /^[0-9a-zA-Z]+$/;
 
 		for (i = 0; i < arLen; i++){
 			sna[i] = nameArr[i] + "=" + valuArr[i];
-
 		}
 		
 		sna.sort();
@@ -53,6 +56,7 @@ var alphanum = /^[0-9a-zA-Z]+$/;
 		document.getElementById("list1").innerHTML = text;
 	});
 
+	//Sort by value
 	$("#sortVal").click(function () {
 		//1. Create Need variables
 		var sva = [];
@@ -84,14 +88,58 @@ var alphanum = /^[0-9a-zA-Z]+$/;
 		document.getElementById("list1").innerHTML = text;	
 	});
 
+	//Delete button
 	$("#delete").click(function () {
-		console.log("Delete Pressed");
-		//$("#list1").remove(); // Just removes entire list emement
 
-		$("#option1").remove(); //Nothing happens
+		var text, delArLen;
+		delArLen = valuArr.length;
+		//Make sure something is selected that can be deleted
+		if (selectedOption == -1) {
+			alert('Please select something to delete');
+		}
+		//Find spot in arrays base on the number clicked and delete them with splice();
+		else {
+			nameArr.splice(selectedOption, 1);
+			valuArr.splice(selectedOption, 1);
 
-		//Give each option a #? And a nuber related to the value?
+			for (var i = 0; i < (delArLen - 1); i++) { //The -1 removes an undefind value apearing at the end
+				text += "<option id=\"option" + i +  "\">" + nameArr[i] + "=" + valuArr[i] + "</option>";
+			}
+			document.getElementById("list1").innerHTML = text;
 
+		}
+	});
+
+	//This function is used to get the option number from the clicked item.
+	$("#list1").click(function () {
+		var i;
+		var arLen = valuArr.length;
+		var arOptions = [];
+		//Get the value of what's written in the option
+		var selectedValue = $("#list1").val();
+		//Combine values to get the options on the screen
+		for (i = 0; i < arLen; i++){
+			arOptions[i] = "\"" + nameArr[i] + "=" + valuArr[i] + "\"";
+		}
+		
+		//Search the array to find out the matching strings position
+		//If not match is found it will be '-1'
+		//If there are more than one entry of the same Name=Value, expect an error for now.
+		selectedOption = arOptions.indexOf("\"" + selectedValue + "\"");
+	});
+
+	//Put data into XML format. Display with Alert. 
+	$("#showXML").click(function () {
+
+		var xml = '<XMLDATA>';
+
+		for(var x = 0; x < nameArr.length; x++){
+			xml += '<pair>' + nameArr[x] + '=' + valuArr[x] + '</pair>';
+		}
+
+		xml += '</XMLDATA>';
+
+    	alert(xml);
 	});
 
 });
